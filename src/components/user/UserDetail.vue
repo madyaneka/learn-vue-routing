8a18f376d9490217d40c34ed57163a45f2c266cb
Madyan Eka <madyan.eka95@gmail.com>
2018-10-19 06:11:44 +0700

    protecting routes with guards / beforeRouteEnter

--- src/components/user/UserDetail.vue
+++ src/components/user/UserDetail.vue
@@ -25,6 +25,14 @@ export default {
                 hash: '#data'
             }
         }
+    },
+
+    beforeRouteEnter(to, from, next) {
+        if (true) {
+            next()
+        } else {
+            next(false)
+        }
     }
 }
 </script>
